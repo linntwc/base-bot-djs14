395fd71e7cfb8f86af8c981b4560cd2db506d96a
twc <115900696+twcln@users.noreply.github.com>
2022-10-19 19:03:15 -0300

    suporte a slash e correções.

--- src/botEvents/messageCreate.js
+++ src/botEvents/messageCreate.js
@@ -1,12 +1,13 @@
 module.exports = async (client, message) => {
   if (message.author.bot || message.channel.type == "DM") return;
-  const botPrefixo = !dbGuild ? process.env.PREFIX : guilddbGuildData?.prefix;
 
   const dbGuild = await client.db.guilds.findOne({_id: message.guild.id,});
   const dbUser = await client.db.users.findOne({_id: message.author.id,});
 
   if (!dbGuild) await client.db.guilds.create({ _id: message.guild.id });
   if (!dbUser) await client.db.users.create({ _id: message.author.id });
+
+  const botPrefixo = !dbGuild ? process.env.PREFIX : dbGuild?.prefix;
 
   if (!message.content.toLowerCase().startsWith(botPrefixo.toLowerCase())) return;
   if (!message.content.startsWith(botPrefixo)) return;
@@ -15,8 +16,8 @@ module.exports = async (client, message) => {
 
   let cmdFind = argsBot.shift().toLowerCase();
   if (cmdFind.length === 0) return;
-  let botCommand = client.botCommands.get(cmdFind);
-  if (!botCommand) botCommand = client.botCommands.get(client.aliases.get(cmdFind));
+  let botCommand = client.commands.get(cmdFind);
+  if (!botCommand) botCommand = client.commands.get(client.aliases.get(cmdFind));
   
   try {
     botCommand.run(client, message, argsBot);
@@ -25,5 +26,3 @@ module.exports = async (client, message) => {
   }
 
 }
-  
-
--- src/botEvents/ready.js
+++ src/botEvents/ready.js
@@ -1,11 +1,15 @@
+const slashArray = require('../main.js');
+
 module.exports = async (client) => {
-    client.user.setStatus("online");
+    await client.user.setStatus("online");
+    await client.application.commands.set(slashArray)
 
     let statusBot = [`Bot legal.`];
     let atividadesBot = ['LISTENING', 'PLAYING', 'WATCHING'];
-
+     
       i = 0;
     setInterval(() => client.user.setActivity(`${statusBot[i++ % statusBot.length]}`, { type: `${atividadesBot[i++ % atividadesBot.length]}`}), 16000);
 
-    console.log(`🤖 | [Bot] Conectado em ${client.user.tag}.\n👥 | [Estatísticas] ${client.guilds.cache.size.toLocaleString()} servidores | ${client.guilds.cache.map(g => g.memberCount).reduce((x, f) => x + f, 0).toLocaleString()} usuários`)
+    console.log(`👥 | [Estatísticas] ${client.guilds.cache.size.toLocaleString()} servidores | ${client.guilds.cache.map(g => g.memberCount).reduce((x, f) => x + f, 0).toLocaleString()} usuários\n🤖 | [Bot] Conectado em ${client.user.tag}.`)
+    console.log('💠 | [Slash Commands] Comandos carregados em todas guilds.');
   }
