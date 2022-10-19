2ec5347dcdc9a5da35f9a9341ca70e8adedeb94c
twc <115900696+twcln@users.noreply.github.com>
2022-10-19 19:25:09 -0300

    linhazinha

--- src/botEvents/messageUpdate.js
+++ src/botEvents/messageUpdate.js
@@ -1,7 +1,6 @@
 module.exports = async (client, oldMessage, newMessage) => {
 
     if (newMessage?.channel?.type == 'DM' || newMessage?.author?.bot) return;
-
     if (oldMessage.content === newMessage.content) return;
 
     client.emit('messageCreate', newMessage);
