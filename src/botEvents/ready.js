cfe6a4e92e115b52ea60e1fe7be8aa525ece24b2
Linn <115900696+twcln@users.noreply.github.com>
2022-11-08 17:03:36 -0300

    v14 update

--- src/botEvents/ready.js
+++ src/botEvents/ready.js
@@ -1,3 +1,4 @@
+const { ActivityType } = require('discord.js');
 const slashArray = require('../main.js');
 
 module.exports = async (client) => {
@@ -5,10 +6,10 @@ module.exports = async (client) => {
     await client.application.commands.set(slashArray)
 
     let statusBot = [`Bot legal.`];
-    let atividadesBot = ['LISTENING', 'PLAYING', 'WATCHING'];
+    let atividadesBot = [ActivityType.Listening, ActivityType.Playing, ActivityType.Watching];
      
       i = 0;
-    setInterval(() => client.user.setActivity(`${statusBot[i++ % statusBot.length]}`, { type: `${atividadesBot[i++ % atividadesBot.length]}`}), 16000);
+    setInterval(() => client.user.setActivity(`${statusBot[i++ % statusBot.length]}`, { type: atividadesBot[i++ % atividadesBot.length]}), 2000);
 
     console.log(`👥 | [Estatísticas] ${client.guilds.cache.size.toLocaleString()} servidores | ${client.guilds.cache.map(g => g.memberCount).reduce((x, f) => x + f, 0).toLocaleString()} usuários\n🤖 | [Bot] Conectado em ${client.user.tag}.`)
     console.log('💠 | [Slash Commands] Comandos carregados em todas guilds.');
